05c5084efd87af44e085a88a30bf1541b4e64f79
Bakshish Singh <bakshish10621@gmail.com>
2024-09-25 22:30:28 +0530

    o auth providers

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,4 @@
-// models/User.ts
+
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -9,10 +9,12 @@ declare module 'next-auth' {
       name?: string | null;
       email?: string | null;
       image?: string | null;
+      role: string;
     } & DefaultSession['user'];
   }
 
   interface User {
     id: string; // Add custom fields here
+    role: string;
   }
 }
